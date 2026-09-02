170fc8252a2e222487be63427add77f96e459ad1
Gustavo <gustavo.souza0911@gmail.com>
2026-09-02 20:52:35 -0300

    adicionado type e convert de dados

--- exemplo1.py
+++ exemplo1.py
@@ -1,5 +1,10 @@
 nome = input(" Qual seu nome ? ")
 print (nome)
 
+idade = int(input("Digite sua idade: "))
+
 print (type(nome))
+print (type(idade))
+
+print("Eu sou o " + nome + " e tenho " + str(idade) + " anos.")
 
