5bf1ae548a388a58375671f99c03f65ebff42bcf
Gustavo Lopes <gustavo.souza0911@gmail.com>
2026-09-03 19:34:40 -0300

    Alterando aula 01

--- aula01/exemplo1.py
+++ aula01/exemplo1.py
@@ -1,10 +1,16 @@
 nome = input(" Qual seu nome ? ")
-print (nome)
-
 idade = int(input("Digite sua idade: "))
 
-print (type(nome))
-print (type(idade))
+# print (type(nome))
+# print (type(idade))
 
 print("Eu sou o " + nome + " e tenho " + str(idade) + " anos.")
 
+
+if(nome == "Gustavo"):
+    print("\nVocê é meu chará!")
+elif(idade == 18):
+    print("Nossa! Temos a mesma idade!")
+
+else:
+    print("Legal!")
