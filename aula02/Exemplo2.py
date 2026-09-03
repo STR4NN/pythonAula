789b9d6bb4e761ddc505c1d958230b7f63e85dd2
Gustavo Lopes <gustavo.souza0911@gmail.com>
2026-09-03 20:56:38 -0300

    aprendendo operadores

---
+++ aula02/Exemplo2.py
@@ -0,0 +1,17 @@
+# Aspas simples ou compostas.
+nome = "Gustavo L"
+curso = 'ADS'
+
+# Concatenar texto no print!
+print("Meu nome é " + nome, " e eu curso " + curso)
+
+# f-string, dentro do texto use {} para variaveis
+print(f"Meu nome é {nome}\nEu curso {curso}")
+
+# Quebra de linha \n
+# Aspas dentro do texto = Dentro de ' "" '
+
+print('Nome: '+ nome + ' "Curso: "' + curso)
+
+# Tabulação = Espaço do TAB
+print("Nome: \t Gustavo\t Sou")
---
+++ aula02/Operadores.py
@@ -0,0 +1,18 @@
+v1 = float(input("Digite um numero de 0 a 10: "))
+
+v2 = float(input("Digite um numero de 0 a 10: "))
+
+resultado = v1 + v2
+print(f"Resultado da soma: {resultado} ")
+
+resultado = v1 - v2
+print(f"Resultado da subtração: {resultado} ")
+
+resultado = v1 * v2
+print(f"Resultado da multiplicação: {resultado} ")
+
+resultado = v1 / v2
+print(f"Resultado da divisão: {resultado} " )
+
+resultado = v1 % v2
+print(f"Resultado do módulo: {resultado}")
