677afd54e9b70278fcfbb212c91194e926f00ca8
Gustavo Lopes <gustavo.souza0911@gmail.com>
2026-09-09 20:08:55 -0300

    Entregando exercicio 9

---
+++ aula03/listadeexercicios/Exercicio9.py
@@ -0,0 +1,10 @@
+print("--------Conversor de horas para minutos e segundos-----------------")
+
+horas = float(input("Digite a quantidade de horas para ser convertida: "))
+print("----------------------------------------------\n")
+
+minutos = horas * 60
+print(horas, " Horas são:  ", minutos, " Minutos\n")
+
+segundos = minutos * 60
+print(horas, " Horas são:", segundos, " Segundos")
